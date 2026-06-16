3675f3833fea0287e8bd04e94c1504a845716de9
Kyeongmin Kim <kkfenix3085@naver.com>
2026-06-16 17:07:48 +0900

    Add SO Class Files

---
+++ BulletSort/Assets/Project/Scripts/Data/WaveData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+// [AUTO GENERATED CLASS] written by GenerateSOClass(CSVParser.cs)
+public partial class WaveData : ScriptableObject
+{
+    public int WaveID;
+    public int WavePattern_1;
+    public int WavePattern_2;
+    public int WavePattern_3;
+    public int WavePattern_4;
+    public int WavePattern_5;
+    public int WavePattern_6;
+    public int WavePattern_7;
+    public int WavePattern_8;
+    public int WavePattern_9;
+}
