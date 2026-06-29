e15a661cde78279f5bdc49c8eb1c65eddbff392a
Kyeongmin Kim <kkfenix3085@naver.com>
2026-06-29 18:00:55 +0900

    Add Exp, Level SO Class

---
+++ BulletSort/Assets/Project/Scripts/Data/ExpData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+// [AUTO GENERATED CLASS] written by GenerateSOClass(CSVParser.cs)
+public partial class ExpData : ScriptableObject
+{
+    public int ExpTypeID;
+    public int ExpAmount;
+    public string BlockSpriteKey;
+}
