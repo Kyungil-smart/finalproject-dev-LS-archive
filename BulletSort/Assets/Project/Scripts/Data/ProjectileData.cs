e8fa502b938f765297858fd5f0969d0c6edfc190
Kyeongmin Kim <kkfenix3085@naver.com>
2026-07-06 17:43:47 +0900

    Add Regular SO Assets : Tower, Piece, Projectile

---
+++ BulletSort/Assets/Project/Scripts/Data/ProjectileData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+// [AUTO GENERATED CLASS] written by GenerateSOClass(CSVParser.cs)
+public partial class ProjectileData : ScriptableObject
+{
+    public int ProjectileID;
+    public int ProjectileType;
+    public string ProjectilePrefab;
+    public string PopEffectPrefab;
+    public string SubPopEffectPrefab;
+}
