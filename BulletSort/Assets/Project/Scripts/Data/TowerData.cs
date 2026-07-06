f693f60850862a7f473dd758408eeda2dd275c33
Kyeongmin Kim <kkfenix3085@naver.com>
2026-07-06 15:42:11 +0900

    Add TowerData CSV, SO

---
+++ BulletSort/Assets/Project/Scripts/Data/TowerData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+// [AUTO GENERATED CLASS] written by GenerateSOClass(CSVParser.cs)
+public partial class TowerData : ScriptableObject
+{
+    public int TowerID;
+    public int TowerType;
+    public int TowerAiType;
+    public int TowerAtk;
+    public float TowerAtkSpeed;
+    public int TowerMaxRange;
+    public int TowerMaxAmmo;
+    public int TowerProjectile;
+    public int ProjectileCount;
+    public float ProjectileSize;
+    public int PiercingCount;
+    public float SplashRadius;
+    public int CurrentHp;
+    public float BulletSpeed;
+    public string TowerTypeText;
+    public string TowerName;
+}
