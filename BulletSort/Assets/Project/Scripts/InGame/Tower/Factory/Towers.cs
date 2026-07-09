507edea6d8487291d4754157e23712c1096962d8
Largegini <dkswjddus0215@gmail.com>
2026-07-09 14:18:28 +0900

    코루틴 안전성 수정

--- BulletSort/Assets/Project/Scripts/InGame/Tower/Factory/Towers.cs
+++ BulletSort/Assets/Project/Scripts/InGame/Tower/Factory/Towers.cs
@@ -57,7 +57,13 @@ namespace Towers.Factory
         {
             if (!gameObject.activeInHierarchy) return;
 
-            StartCoroutine(Attack());
+            if (_atkCoroutine != null)
+            {
+                StopCoroutine(_atkCoroutine);
+                _atkCoroutine = null;
+            }
+
+            _atkCoroutine = StartCoroutine(Attack());
         }
 
         // 공격 코루틴
@@ -90,7 +96,7 @@ namespace Towers.Factory
                 if (!_isOverSorting)
                     yield return new WaitUntil(() => _targetDetector.target != null);
             }
-
+            
             Destroy(gameObject);
         }
 
@@ -122,13 +128,12 @@ namespace Towers.Factory
             }
 
             _towerInfo = _towerInfoOrigin.UpdateInfo(bonus);
-
-            return;
         }
 
 
         private void OnDestroy()
         {
+            StopCoroutine(_atkCoroutine);
             _atkCoroutine = null;
             _currentAmmo = 0;
             _slotTurretQueue.NotifyTurretDestroyed(this);
